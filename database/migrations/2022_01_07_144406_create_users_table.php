357fb4db02337e438295db9a17fd340307200d10
AlexTuersley <alex.tuersley@simplythankyou.co.uk>
2022-01-07 14:50:00 +0000

    Users Tables added

--- database/migrations/2022_01_07_144406_create_users_table.php
+++ database/migrations/2022_01_07_144406_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->stirng('email')->unique();
+            $table->string('email')->unique();
             $table->string('name');
             $table->string('password');
             $table->timestamps();
